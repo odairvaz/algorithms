a68c4e55bd537d2fe82bc6906dd9fe8788407131
Oda <odairvaz>
2022-11-23 17:17:18 +0100

    Array Partition I

---
+++ src/com/leetcode/easy/ArrayPartition1.java
@@ -0,0 +1,17 @@
+package com.leetcode.easy;
+
+import java.util.Arrays;
+
+public class ArrayPartition1 {
+
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+
+        int maxSum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            maxSum += nums[i];
+        }
+        return maxSum;
+    }
+
+}
