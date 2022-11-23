a48bf3a103ed5a05fb6f656249add5169b3d089a
Oda <odairvaz>
2022-11-23 18:07:53 +0100

    Two Sum II

---
+++ src/com/leetcode/medium/TwoSum2.java
@@ -0,0 +1,17 @@
+package com.leetcode.medium;
+
+public class TwoSum2 {
+
+    public  int[] twoSum(int[] numbers, int target) {
+
+        int l = 0, r = numbers.length - 1;
+
+        while (numbers[l] + numbers[r] != target) {
+            if (numbers[l] + numbers[r] < target) l++;
+            else r--;
+        }
+
+        return new int[] {l+1, r+1};
+    }
+
+}
