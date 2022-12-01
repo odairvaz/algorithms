5e505410fe08b0bae21ed81ae446d84931b5eda8
Oda <odairvaz>
2022-12-01 09:38:45 +0100

    Move zeroes

---
+++ src/com/leetcode/easy/MoveZeroes.java
@@ -0,0 +1,20 @@
+package com.leetcode.easy;
+
+public class MoveZeroes {
+
+    public static void moveZeroes(int[] nums) {
+        int leftMostZeroIndex = 0; // The index of the leftmost zero
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i > leftMostZeroIndex) { // There are zero(s) on the left side of the current non-zero number, swap!
+                    nums[leftMostZeroIndex] = nums[i];
+                    nums[i] = 0;
+                }
+
+                leftMostZeroIndex++;
+            }
+        }
+    }
+
+
+}
