fa2bd369db7b2821c15c0bb78cf6efc7a6c99c01
Oda <odairvaz>
2022-12-05 17:37:31 +0100

    Intersection of two arrays

---
+++ src/com/leetcode/easy/IntersectionTwoArrays.java
@@ -0,0 +1,20 @@
+package com.leetcode.easy;
+
+import java.util.HashSet;
+
+public class IntersectionTwoArrays {
+
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        for (Integer n : nums1) set1.add(n);
+        HashSet<Integer> set2 = new HashSet<>();
+        for (Integer n : nums2) set2.add(n);
+
+        set1.retainAll(set2);
+
+        int [] output = new int[set1.size()];
+        int idx = 0;
+        for (int s : set1) output[idx++] = s;
+        return output;
+    }
+}
