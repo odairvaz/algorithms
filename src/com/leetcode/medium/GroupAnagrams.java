5615b671b0b4bba7fdb2cd3fd77adc01f64417b5
Oda <odairvaz>
2022-12-14 11:17:28 +0100

    49. Group Anagrams

---
+++ src/com/leetcode/medium/GroupAnagrams.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> groups = new HashMap<>();
+        for ( String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+
+            String key = String.valueOf(chars);
+            groups.putIfAbsent(key, new ArrayList<>());
+            groups.get(key).add(s);
+        }
+        return new ArrayList<>(groups.values());
+    }
+
+}
